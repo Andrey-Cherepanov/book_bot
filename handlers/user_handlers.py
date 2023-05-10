bd8c0c19ebba978483de5c34a623ad75df2bcc87
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-10 11:36:49 +0500

    start command handler

---
+++ handlers/user_handlers.py
@@ -0,0 +1,18 @@
+from aiogram import Router
+from aiogram.filters import Text, Command, CommandStart
+from aiogram.types import CallbackQuery, Message
+
+from database import database
+from filters.filters import IsDigitCallbackData, IsDelBookmarkCallbackData
+from keyboards.bookmark_kb import create_bookmarks_keyboard, create_edit_keyboard
+from keyboards.pagination_kb import create_pagination_keyboard
+from lexicon.lexicon import LEXICON
+from services.file_handling import book
+
+router = Router()
+
+# Command /start
+@router.message(CommandStart())
+async def process_start_command(message):
+    await message.answer(LEXICON['/start'])
+    database.add_user(message.from_user.id)
