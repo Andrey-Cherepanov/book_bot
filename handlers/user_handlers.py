8cdb45da4103e2469ed8962b1f919c52884e2364
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-10 12:27:18 +0500

    handlers for /help and /beginning

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -16,3 +16,23 @@ router = Router()
 async def process_start_command(message):
     await message.answer(LEXICON['/start'])
     database.add_user(message.from_user.id)
+
+# Command /help
+@router.message(Command(commands='help'))
+async def process_help_command(message):
+    await message.answer(LEXICON['/help'])
+
+# Command /beginning
+# sending user first page
+@router.message(Command(commands='beginning'))
+async def process_beginning_command(message):
+    text = book[1]
+    id = message.from_user.id
+    await message.answer(
+            text=text,
+            reply_markup=create_pagination_keyboard(
+                'backward',
+                f'{database.get_current_page(id)}/{len(book)}',
+                'forward'
+            )
+    )
