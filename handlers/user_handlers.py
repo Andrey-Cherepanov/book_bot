7b5dd9297a47dea4106dbc64f530188362c30339
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2023-05-10 12:22:41 +0500

    fixed import

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,7 +4,7 @@ from aiogram.types import CallbackQuery, Message
 
 from database import database
 from filters.filters import IsDigitCallbackData, IsDelBookmarkCallbackData
-from keyboards.bookmark_kb import create_bookmarks_keyboard, create_edit_keyboard
+from keyboards.bookmarks_kb import create_bookmarks_keyboard, create_edit_keyboard
 from keyboards.pagination_kb import create_pagination_keyboard
 from lexicon.lexicon import LEXICON
 from services.file_handling import book
